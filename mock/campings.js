901b719334e77ee521a0ff9904d4b6770be7c217
verbaaal <verbaaal@gmail.com>
2019-12-02 16:19:44 +0100

    first commit

---
+++ mock/campings.js
@@ -0,0 +1,34 @@
+const campings = [
+  {
+    id: 1,
+    type: 'rv',
+    name: 'RAPTA CHEZ MARC',
+    description: 'VENEZ NOMBREUX',
+    rating: 4.9,
+    distance: 2.9,
+    price: 'Free',
+    image: 'https://images.unsplash.com/photo-1525811902-f2342640856e?fit=crop&w=900&h=600&q=130',
+    latlng: {
+      latitude: 37.79335,
+      longitude: -122.4424,
+    }
+  },
+  {
+    id: 2,
+    type: 'tent',
+    name: 'RAPTA CHEZ SIMON',
+    description: 'je suis bg x.',
+    rating: 4.9,
+    distance: 2.9,
+    price: 'Free',
+    image: 'https://images.unsplash.com/photo-1506535995048-638aa1b62b77?fit=crop&w=900&h=600&q=130',
+    latlng: {
+      latitude: 37.78865,
+      longitude: -122.4324,
+    }
+  },
+];
+
+export {
+  campings,
+};
---
+++ navigation/ScreensNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation';
+
+import Campings from '../screens/Campings';
+import Settings from '../screens/Settings';
+
+
+
+export default createStackNavigator({
+  Campings: Campings,
+  Settings: Settings,
+ 
+});
