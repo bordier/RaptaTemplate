6c0251f1cfb03baab0025f21df6fec506e2ab037
verbaaal <verbaaal@gmail.com>
2019-12-05 15:58:16 +0100

    avec localisation

--- mock/campings.js
+++ mock/campings.js
@@ -13,8 +13,25 @@ const campings = [
       longitude: -122.4424,
     }
   },
+
+
   {
     id: 2,
+    type: 'rv',
+    name: 'RAPTA CHEZ MARC',
+    description: 'VENEZ NOMBREUX',
+    rating: 4.9,
+    distance: 2.9,
+    price: 'Free',
+    image: 'https://images.unsplash.com/photo-1525811902-f2342640856e?fit=crop&w=900&h=600&q=130',
+    latlng: {
+      latitude: 39.79335,
+      longitude: -122.4424,
+    }
+  },
+
+  {
+    id: 3,
     type: 'tent',
     name: 'RAPTA CHEZ SIMON',
     description: 'je suis bg x.',
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
   };
 
   render() {
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{text}</Text>
+      </View>
     );
   }
 }
@@ -21,7 +51,14 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
